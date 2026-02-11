91cac3881398dc3c2c7622b06384aca3d9cf1d5d
chadhas282-afk <chadhas282@gmail.com>
2026-02-11 15:23:20 +0530

    add logic to delete button

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function appendToDisplay(value) {
     }
 }
 
-// function clearDisplay() {
-//     document.getElementById('display').value = '';
-// }
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
-// function calculateResult() {
-//     const display = document.getElementById('display');
-//     try {
-//         if (display.value.trim() === "") return;
-//         display.value = eval(display.value);
-//     } catch (error) {
-//         display.value = 'Error';
-//     }
-// }
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        if (display.value.trim() === "") return;
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
